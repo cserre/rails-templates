ca24913864468de06f87702ac5d9edec5fcbb75a
Charlotte Serre <charlotte@lokora.co>
2016-09-07 16:30:41 +0200

    Update and rename devise.rb to devise-rails4.rb

--- devise-rails4.rb
+++ devise-rails4.rb
@@ -1,56 +1,54 @@
-run "pgrep spring | xargs kill -9"
 run "rm Gemfile"
 file 'Gemfile', <<-RUBY
 source 'https://rubygems.org'
-ruby '#{RUBY_VERSION}'
-
-gem 'rails', '#{Rails.version}'
-gem 'puma'
+ruby '2.3.0'
+gem 'rails', '4.2.5.1'
 gem 'pg'
 gem 'figaro'
 gem 'jbuilder', '~> 2.0'
-gem 'devise'#{Rails.version >= "5" ? ", github: 'plataformatec/devise'" : nil}
-gem 'redis'
-
-gem 'sass-rails'
+gem 'devise'
+gem 'sass-rails', '~> 5.0'
 gem 'jquery-rails'
 gem 'uglifier'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
-
 group :development, :test do
   gem 'binding_of_caller'
   gem 'better_errors'
-  #{Rails.version >= "5" ? nil : "gem 'quiet_assets'"}
+  gem 'quiet_assets'
   gem 'pry-byebug'
   gem 'pry-rails'
   gem 'spring'
-  #{Rails.version >= "5" ? "gem 'listen', '~> 3.0.5'" : nil}
-  #{Rails.version >= "5" ? "gem 'spring-watcher-listen', '~> 2.0.0'" : nil}
 end
-
-#{Rails.version < "5" ? "gem 'rails_12factor', group: :production" : nil}
+group :production do
+  gem 'rails_12factor'
+  gem 'puma'
+end
 RUBY
-
-file ".ruby-version", RUBY_VERSION
 
 file 'Procfile', <<-YAML
 web: bundle exec puma -C config/puma.rb
 YAML
 
-if Rails.version < "5"
-puma_file_content = <<-RUBY
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-
-threads     threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
+file 'config/puma.rb', <<-RUBY
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 3)
+threads threads_count, threads_count
+preload_app!
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
 RUBY
 
-file 'config/puma.rb', puma_file_content, force: true
-end
+generate(:controller, 'pages', 'home', '--no-helper', '--no-assets', '--skip-routes')
+route "root to: 'pages#home'"
 
 run "rm -rf app/assets/stylesheets"
 run "curl -L https://github.com/lewagon/stylesheets/archive/master.zip > stylesheets.zip"
@@ -64,19 +62,16 @@ file 'app/assets/javascripts/application.js', <<-JS
 //= require_tree .
 JS
 
-gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
-
 run 'rm app/views/layouts/application.html.erb'
 file 'app/views/layouts/application.html.erb', <<-HTML
 <!DOCTYPE html>
 <html>
   <head>
     <title>TODO</title>
+    <%= stylesheet_link_tag    'application', media: 'all' %>
     <%= csrf_meta_tags %>
-    #{Rails.version >= "5" ? "<%= action_cable_meta_tag %>" : nil}
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <%= stylesheet_link_tag    'application', media: 'all' %>
   </head>
   <body>
     <%= render 'shared/navbar' %>
@@ -103,19 +98,13 @@ file 'app/views/shared/_flashes.html.erb', <<-HTML
 HTML
 
 run "curl -L https://raw.githubusercontent.com/lewagon/awesome-navbars/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb"
-run "curl -L https://raw.githubusercontent.com/lewagon/design/master/logos/png/logo_red_circle.png > app/assets/images/logo.png"
 
-run "rm README.rdoc"
-markdown_file_content = <<-MARKDOWN
-Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+run "README.rdoc"
+file 'README.md', <<-MARKDOWN
+Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates)
 MARKDOWN
-file 'README.md', markdown_file_content, force: true
 
 after_bundle do
-  rake 'db:drop db:create db:migrate'
-  generate('simple_form:install', '--bootstrap')
-  generate(:controller, 'pages', 'home', '--no-assets', '--skip-routes')
-  route "root to: 'pages#home'"
   run "rm .gitignore"
   file '.gitignore', <<-TXT
 .bundle
@@ -126,29 +115,14 @@ tmp/*
 .DS_Store
 public/assets
 TXT
+  run "bundle exec figaro install"
+  generate('simple_form:install', '--bootstrap')
   generate('devise:install')
   generate('devise', 'User')
-  run 'rm app/controllers/application_controller.rb'
-  file 'app/controllers/application_controller.rb', <<-RUBY
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
-end
-RUBY
-  rake 'db:migrate'
   generate('devise:views')
-  run 'rm app/controllers/pages_controller.rb'
-  file 'app/controllers/pages_controller.rb', <<-RUBY
-class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
-  def home
-  end
-end
-RUBY
   environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
   environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
-  run "figaro install"
+  rake 'db:drop db:create db:migrate'
   git :init
   git add: "."
   git commit: %Q{ -m 'Initial commit with devise template from https://github.com/lewagon/rails-templates' }
