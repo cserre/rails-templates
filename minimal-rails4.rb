7c01e3828b191b318b6c57c0c079881339abd720
Charlotte Serre <charlotte@lokora.co>
2016-09-07 15:33:46 +0200

    minimal on rails 4

---
+++ minimal-rails4.rb
@@ -0,0 +1,104 @@
+run "rm Gemfile"
+file 'Gemfile', <<-RUBY
+source 'https://rubygems.org'
+ruby '2.3.0'
+gem 'rails', '4.2.5.1'
+gem 'pg'
+gem 'figaro'
+gem 'jbuilder', '~> 2.0'
+gem 'sass-rails', '~> 5.0'
+gem 'jquery-rails'
+gem 'uglifier'
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+group :development, :test do
+  gem 'binding_of_caller'
+  gem 'better_errors'
+  gem 'quiet_assets'
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'spring'
+end
+group :production do
+  gem 'rails_12factor'
+  gem 'puma'
+end
+RUBY
+
+file 'Procfile', <<-YAML
+web: bundle exec puma -C config/puma.rb
+YAML
+
+file 'config/puma.rb', <<-RUBY
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 3)
+threads threads_count, threads_count
+preload_app!
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+RUBY
+
+generate(:controller, 'pages', 'home', '--no-helper', '--no-assets', '--skip-routes')
+route "root to: 'pages#home'"
+
+run "rm -rf app/assets/stylesheets"
+run "curl -L https://github.com/lewagon/stylesheets/archive/master.zip > stylesheets.zip"
+run "unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets"
+
+run 'rm app/assets/javascripts/application.js'
+file 'app/assets/javascripts/application.js', <<-JS
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require_tree .
+JS
+
+run 'rm app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.erb', <<-HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO</title>
+    <%= stylesheet_link_tag    'application', media: 'all' %>
+    <%= csrf_meta_tags %>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  </head>
+  <body>
+    <%= yield %>
+    <%= javascript_include_tag 'application' %>
+  </body>
+</html>
+HTML
+
+run "README.rdoc"
+file 'README.md', <<-MARKDOWN
+Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates)
+MARKDOWN
+
+after_bundle do
+  run "rm .gitignore"
+  file '.gitignore', <<-TXT
+.bundle
+log/*.log
+tmp/**/*
+tmp/*
+*.swp
+.DS_Store
+public/assets
+TXT
+  run "bundle exec figaro install"
+  generate('simple_form:install', '--bootstrap')
+  rake 'db:drop db:create db:migrate'
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit with minmal template from https://github.com/lewagon/rails-templates' }
+end
